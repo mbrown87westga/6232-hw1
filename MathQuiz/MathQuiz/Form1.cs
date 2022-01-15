a94740cede13ce2277219ad4084f353cb7a75344
Mike Brown <MBrown3@paylocity.com>
2022-01-15 04:25:34 -0700

    finishing step 2: adding the randomizer and wiring up the start button

--- MathQuiz/MathQuiz/Form1.cs
+++ MathQuiz/MathQuiz/Form1.cs
@@ -12,9 +12,31 @@ namespace MathQuiz
 {
   public partial class Form1 : Form
   {
+    Random randomizer = new Random();
+
+    int addend1;
+    int addend2;
+
     public Form1()
     {
       InitializeComponent();
+    }
+
+    public void StartTheQuiz()
+    {
+      addend1 = randomizer.Next(51);
+      addend2 = randomizer.Next(51);
+
+      plusLeftLabel.Text = addend1.ToString();
+      plusRightLabel.Text = addend2.ToString();
+      
+      sum.Value = 0;
+    }
+
+    private void startButton_Click(object sender, EventArgs e)
+    {
+      StartTheQuiz();
+      startButton.Enabled = false;
     }
   }
 }
